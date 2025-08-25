24eaaf2895a155c95942bf566e4729d09ef90f07
sridila <mostafaalaisswi70@gmail.com>
2025-08-25 12:43:50 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+  // مثال: نافذة منبثقة عند دخول الصفحة
+  alert("هاته الصفحة قيد التطوير نتاسف لاي خطا في الصفحة لمزيد من المعلومات راسلنا على وتساب!");
+};window.onload = function() {
+  document.getElementById('popup').style.display = 'flex';
+};
+function closePopup() {
+  document.getElementById('popup').style.display = 'none';
+}
